b831c4ba4677a1aa2b335b6a808d09e8800be216
MDFLAMorANGELS <valentinmd78@gmail.com>
2023-04-17 18:50:22 +0200

    correction 03

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,7 +1,7 @@
 def who_is_bigger(a, b, c)
     if a == nil || b == nil || c == nil
        "nil detected"
-    else a > b && a > c
+    elsif a > b && a > c
        "a is bigger"
     elsif b > a && b > c
        "b is bigger"
